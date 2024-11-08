af1f85de14cbd5f6da368b780f6d14cf0c508595
vmarialuzm <vmarialuzm@gmail.com>
2024-11-07 21:02:17 -0500

    change console.log por console.error

--- src/controllers/trajectories-controller.ts
+++ src/controllers/trajectories-controller.ts
@@ -38,7 +38,7 @@ export const trajectories = async(req: Request, res: Response) => {
 
         return res.status(200).json(trajectories);
     } catch (error) {
-        console.log("Error al listar los trajectories: ", error);
+        console.error("Error al listar los trajectories: ", error);
         return res.status(500).json({
             error: "Error interno del servidor",
         })
