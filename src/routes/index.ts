f323e1b3dc06fc6fba4db046e9f80897ff3dfb57
vmarialuzm <vmarialuzm@gmail.com>
2024-11-08 11:33:45 -0500

    add endpoint list latest trajectories for taxi

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import taxisRouter from './taxis-routes';
 import trajectoriesRouter from './trajectories-routes';
+import trajectoriesLatestRouter from './trajectories-latest-router';
 
 const rootRouter: Router = Router()
 
 rootRouter.use('/taxis', taxisRouter);
 rootRouter.use('/trajectories', trajectoriesRouter);
+rootRouter.use('/trajectories/latest', trajectoriesLatestRouter);
 
 export default rootRouter;
